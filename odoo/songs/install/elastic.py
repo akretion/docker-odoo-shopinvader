224726ecb7d098307c8ca34c1a289d9104ba7a1f
Sébastien BEAU <sebastien.beau@akretion.com>
2019-05-29 09:37:44 +0200

    [IMP] finish elastic configuration

--- odoo/songs/install/elastic.py
+++ odoo/songs/install/elastic.py
@@ -3,5 +3,9 @@ import anthem
 
 @anthem.log
 def setup(ctx):
-    backend = ctx.env.ref('shopinvader_elasticsearch.shopinvader.backend_1')
+    backend = ctx.env.ref('shopinvader.backend_1')
     backend.elasticsearch_server_ip = 'elastic'
+    bindings = ctx.env['shopinvader.variant'].search([])
+    bindings.recompute_json(force_export=True)
+    bindings = ctx.env['shopinvader.category'].search([])
+    bindings.recompute_json(force_export=True)
